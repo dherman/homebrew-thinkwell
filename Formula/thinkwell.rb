a56125ea9d663c2f2d13aca8203697cd387608cf
David Herman <david.herman@gmail.com>
2026-02-01 20:52:57 -0800

    feat: add SHA256 checksums for v0.3.0-alpha.1

--- Formula/thinkwell.rb
+++ Formula/thinkwell.rb
@@ -7,22 +7,22 @@ class Thinkwell < Formula
   on_macos do
     on_arm do
       url "https://github.com/dherman/thinkwell/releases/download/v0.3.0-alpha.1/thinkwell-darwin-arm64.tar.gz"
-      sha256 "PLACEHOLDER_DARWIN_ARM64"
+      sha256 "babf755639b63b97e3b8da2f1f5099f792789331f8a7e19d1fb4cc9dcee0f62c"
     end
     on_intel do
       url "https://github.com/dherman/thinkwell/releases/download/v0.3.0-alpha.1/thinkwell-darwin-x64.tar.gz"
-      sha256 "PLACEHOLDER_DARWIN_X64"
+      sha256 "a95b2744d8a513d41497f01a43a2c436b1ed6f872b9a24465fa5ce5a8ca11de2"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/dherman/thinkwell/releases/download/v0.3.0-alpha.1/thinkwell-linux-arm64.tar.gz"
-      sha256 "PLACEHOLDER_LINUX_ARM64"
+      sha256 "974a8d02251638dbfb622838bfd6f133a0063e93018558fbb6882defc8bf7008"
     end
     on_intel do
       url "https://github.com/dherman/thinkwell/releases/download/v0.3.0-alpha.1/thinkwell-linux-x64.tar.gz"
-      sha256 "PLACEHOLDER_LINUX_X64"
+      sha256 "cbd9a694b3a3bb08d5b6e3f7da12b4b0610c707e608fdfb561c6c583d4407d91"
     end
   end
 
